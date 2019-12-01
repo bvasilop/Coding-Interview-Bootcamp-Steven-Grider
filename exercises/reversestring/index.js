21c833572e245dce02f589369d930c0374c24437
bvasilop <bvasilop@gmail.com>
2019-11-30 17:04:05 -0800

    added reverse string algorithm

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
 // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
@@ -6,6 +8,17 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  str = str.toLowerCase();
+  str = [...str];
+
+  for (let i = 0; i < str.length / 2; i++) {
+    const currentLetter = str[i];
+    str[i] = str[str.length - 1 - i];
+    str[str.length - 1 - i] = currentLetter;
+  }
+  return str.join('');
+}
+reverse('Hello world');
 
 module.exports = reverse;
